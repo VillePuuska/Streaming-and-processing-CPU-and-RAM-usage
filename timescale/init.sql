1743779b1cbc3cdabd8bb5c90723f5964fa6fbb2
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-05-05 13:14:20 +0000

    add views in db for streamlit app to use

--- timescale/init.sql
+++ timescale/init.sql
@@ -13,3 +13,28 @@ CREATE TABLE measurements (
 );
 
 SELECT create_hypertable('measurements', by_range('window_start', INTERVAL '1 day'));
+
+CREATE OR REPLACE VIEW latest_measurements AS (
+    WITH last_time AS (
+        SELECT
+            machine_id,
+            measurement_name,
+            MAX(window_time) AS window_time
+        FROM measurements
+        GROUP BY
+            machine_id,
+            measurement_name
+    )
+    SELECT m.*
+    FROM measurements m
+    INNER JOIN last_time l
+    ON m.machine_id = l.machine_id
+    AND m.measurement_name = l.measurement_name
+    AND m.window_time = l.window_time
+);
+
+CREATE OR REPLACE VIEW measurements_last_minute AS (
+    SELECT *
+    FROM measurements
+    WHERE window_time >= now() - INTERVAL '1 minute' -- only the last minute
+);
