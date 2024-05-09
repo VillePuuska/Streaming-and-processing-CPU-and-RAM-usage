a5cda96a2c3af7aeab24c2a9310d3febce02ca85
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-05-09 10:16:34 +0000

    refactor using context manager for duckdb to postgres connections

--- app/utils/db_operations.py
+++ app/utils/db_operations.py
@@ -7,28 +7,24 @@ def get_latest_data(pg_conn: str) -> pd.DataFrame:
     Function returns a Pandas dataframe of the latest average measurements
     in Postgres/Timescale from the view latest_measurements_avg.
     """
-    try:
-        duckdb.sql(f"ATTACH '{pg_conn}' AS pg (TYPE POSTGRES)")
-        res = duckdb.sql(
-            """
-            FROM pg.public.latest_measurements_avg
-            ORDER BY machine_id, measurement_name
-            """
-        ).df()
-    except:
-        return pd.DataFrame(
-            {
-                "machine_id": [],
-                "measurement_name": [],
-                "window_start": [],
-                "avg_value": [],
-            }
-        )
-    finally:
+    with duckdb.connect() as conn:
         try:
-            duckdb.sql("DETACH pg")
+            conn.sql(f"ATTACH '{pg_conn}' AS pg (TYPE POSTGRES)")
+            res = conn.sql(
+                """
+                FROM pg.public.latest_measurements_avg
+                ORDER BY machine_id, measurement_name
+                """
+            ).df()
         except:
-            pass
+            return pd.DataFrame(
+                {
+                    "machine_id": [],
+                    "measurement_name": [],
+                    "window_start": [],
+                    "avg_value": [],
+                }
+            )
     return res
 
 
@@ -38,39 +34,35 @@ def get_last_minute_data(pg_conn: str) -> tuple[pd.DataFrame, pd.DataFrame]:
     last minute of CPU usage measurements and the second one contains the
     last minute of RAM usage measurements.
     """
-    try:
-        duckdb.sql(f"ATTACH '{pg_conn}' AS pg (TYPE POSTGRES)")
-        duckdb.sql(
-            """
-            CREATE OR REPLACE TABLE last_min AS
-            FROM pg.public.measurements_last_minute
-            """
-        )
-        res_cpu = duckdb.sql(
-            """
-            FROM last_min
-            WHERE measurement_name = 'cpu_usage'
-            """
-        ).df()
-        res_ram = duckdb.sql(
-            """
-            FROM last_min
-            WHERE measurement_name = 'memory_usage'
-            """
-        ).df()
-    except:
-        res = pd.DataFrame(
-            {
-                "machine_id": [],
-                "measurement_name": [],
-                "window_start": [],
-                "avg_value": [],
-            }
-        )
-        return res, res
-    finally:
+    with duckdb.connect() as conn:
         try:
-            duckdb.sql("DETACH pg")
+            conn.sql(f"ATTACH '{pg_conn}' AS pg (TYPE POSTGRES)")
+            conn.sql(
+                """
+                CREATE OR REPLACE TABLE last_min AS
+                FROM pg.public.measurements_last_minute
+                """
+            )
+            res_cpu = conn.sql(
+                """
+                FROM last_min
+                WHERE measurement_name = 'cpu_usage'
+                """
+            ).df()
+            res_ram = conn.sql(
+                """
+                FROM last_min
+                WHERE measurement_name = 'memory_usage'
+                """
+            ).df()
         except:
-            pass
+            res = pd.DataFrame(
+                {
+                    "machine_id": [],
+                    "measurement_name": [],
+                    "window_start": [],
+                    "avg_value": [],
+                }
+            )
+            return res, res
     return res_cpu, res_ram
