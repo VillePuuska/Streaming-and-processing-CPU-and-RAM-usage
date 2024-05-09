d00262cc0596f6a4610e1703fa63bcbbcf9cf38c
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-05-09 10:17:55 +0000

    fix cpu usage readings by using cpu_percent() instead of 100.0 - cpu_times_percent().idle

--- measurement_spammer/utils/readings.py
+++ measurement_spammer/utils/readings.py
@@ -5,7 +5,6 @@ def get_readings() -> dict[str, float]:
     res = {}
     res["memory_usage"] = psutil.virtual_memory().percent
 
-    cpu_usage = psutil.cpu_times_percent()
-    res["cpu_usage"] = 100.0 - cpu_usage.idle
+    res["cpu_usage"] = psutil.cpu_percent()
 
     return res
