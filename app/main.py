2cf7220f9e495f5527d522f5c4100e77b424bd0c
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-05-10 14:29:49 +0000

    performance test with keeping the postgres connection open: 10X faster

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import time
 import datetime
-from utils.db_operations import get_latest_data, get_last_minute_data
+import duckdb
+from utils.db_operations import get_latest_data_alt, get_last_minute_data_alt
 
 REFRESH_INTERVAL = 1.0
 PG_CONN = "postgresql://postgres:postgres@localhost:5432/"
@@ -19,31 +20,43 @@ timestamp = st.empty()
 
 time_diff = st.empty()
 
-while True:
-    start_time = time.time()
+with duckdb.connect() as conn:
+    conn.sql(f"ATTACH '{PG_CONN}' AS pg (TYPE POSTGRES)")
+    while True:
+        start_time = time.time()
 
-    latest_data = get_latest_data(pg_conn=PG_CONN)
-    last_minute_data_cpu, last_minute_data_ram = get_last_minute_data(pg_conn=PG_CONN)
+        latest_data = get_latest_data_alt(conn=conn)
+        last_minute_data_cpu, last_minute_data_ram = get_last_minute_data_alt(conn=conn)
 
-    last_minute_data_cpu["machine_id"] = last_minute_data_cpu["machine_id"].astype(str)
-    last_minute_data_ram["machine_id"] = last_minute_data_ram["machine_id"].astype(str)
+        last_minute_data_cpu["machine_id"] = last_minute_data_cpu["machine_id"].astype(
+            str
+        )
+        last_minute_data_ram["machine_id"] = last_minute_data_ram["machine_id"].astype(
+            str
+        )
 
-    header1.header("Last minute CPU usage:")
-    chart1.line_chart(
-        data=last_minute_data_cpu, x="window_start", y="avg_value", color="machine_id"
-    )
+        header1.header("Last minute CPU usage:")
+        chart1.line_chart(
+            data=last_minute_data_cpu,
+            x="window_start",
+            y="avg_value",
+            color="machine_id",
+        )
 
-    header2.header("Last minute RAM usage:")
-    chart2.line_chart(
-        data=last_minute_data_ram, x="window_start", y="avg_value", color="machine_id"
-    )
+        header2.header("Last minute RAM usage:")
+        chart2.line_chart(
+            data=last_minute_data_ram,
+            x="window_start",
+            y="avg_value",
+            color="machine_id",
+        )
 
-    header3.header("Latest data:")
-    dataframe3.dataframe(data=latest_data, hide_index=True)
+        header3.header("Latest data:")
+        dataframe3.dataframe(data=latest_data, hide_index=True)
 
-    ts = datetime.datetime.now()
-    timestamp.caption(f"Data last updated: {ts.strftime('%d.%m.%Y %H:%M:%S')}")
+        ts = datetime.datetime.now()
+        timestamp.caption(f"Data last updated: {ts.strftime('%d.%m.%Y %H:%M:%S')}")
 
-    time_diff.caption(f"Took {time.time()-start_time:.4f} sec to refresh.")
+        time_diff.caption(f"Took {time.time()-start_time:.4f} sec to refresh.")
 
-    time.sleep(max(0.0, start_time + REFRESH_INTERVAL - time.time()))
+        time.sleep(max(0.0, start_time + REFRESH_INTERVAL - time.time()))
--- app/utils/db_operations.py
+++ app/utils/db_operations.py
@@ -66,3 +66,67 @@ def get_last_minute_data(pg_conn: str) -> tuple[pd.DataFrame, pd.DataFrame]:
             )
             return res, res
     return res_cpu, res_ram
+
+
+def get_latest_data_alt(conn: duckdb.DuckDBPyConnection) -> pd.DataFrame:
+    """
+    Function returns a Pandas dataframe of the latest average measurements
+    in Postgres/Timescale from the view latest_measurements_avg.
+    """
+    try:
+        res = conn.sql(
+            """
+            FROM pg.public.latest_measurements_avg
+            ORDER BY machine_id, measurement_name
+            """
+        ).df()
+    except:
+        return pd.DataFrame(
+            {
+                "machine_id": [],
+                "measurement_name": [],
+                "window_start": [],
+                "avg_value": [],
+            }
+        )
+    return res
+
+
+def get_last_minute_data_alt(
+    conn: duckdb.DuckDBPyConnection,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Function returns two Pandas dataframes as a tuple. First one contains the
+    last minute of CPU usage measurements and the second one contains the
+    last minute of RAM usage measurements.
+    """
+    try:
+        conn.sql(
+            """
+            CREATE OR REPLACE TABLE last_min AS
+            FROM pg.public.measurements_last_minute
+            """
+        )
+        res_cpu = conn.sql(
+            """
+            FROM last_min
+            WHERE measurement_name = 'cpu_usage'
+            """
+        ).df()
+        res_ram = conn.sql(
+            """
+            FROM last_min
+            WHERE measurement_name = 'memory_usage'
+            """
+        ).df()
+    except:
+        res = pd.DataFrame(
+            {
+                "machine_id": [],
+                "measurement_name": [],
+                "window_start": [],
+                "avg_value": [],
+            }
+        )
+        return res, res
+    return res_cpu, res_ram
